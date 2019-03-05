2c82ee9ade6bfac3b74c2ad26b4248d568da8e9e
Althea <althea@metasolutions.se>
2019-03-05 17:01:53 +0100

    Add header to projects and fix media queries

--- js/filter.js
+++ js/filter.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
 $(function() {
-  var selectedClass = "all";
-
-   
+  var selectedClass = "all";   
     $(".btn").click(function(){
+      selectedClass = $(this).attr("data-rel");
       if (selectedClass != "all"){
         $(".btn--all").removeClass("btn--active");
-        console.log('hi');
       }
-    selectedClass = $(this).attr("data-rel");
 $("#portfolio").fadeTo(100, 0.1);
     $("#portfolio a").not("."+selectedClass).fadeOut();
 setTimeout(function() {
